d35ada73e39d006af032dd4f8a53c669cef26f4e
Espen Braastad <espebra@ifi.uio.no>
2015-12-13 16:02:52 +0100

    Use the fastest algorithm in the library for resizing

--- app/model/file.go
+++ app/model/file.go
@@ -336,7 +336,7 @@ func (f *File) GenerateThumbnail() error {
 		return err
 	}
 
-	thumb := imaging.Fill(s, 75, 75, imaging.Center, imaging.Lanczos)
+	thumb := imaging.Fill(s, 75, 75, imaging.Center, imaging.NearestNeighbor)
 	err = imaging.Save(thumb, f.ThumbnailPath())
 	return err
 }
