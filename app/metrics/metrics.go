68b9dddc168ae5c3047197b2283acb4b66eff465
Espen Braastad <espebra@ifi.uio.no>
2016-05-08 00:35:23 +0200

    Add metrics package

---
+++ app/metrics/metrics.go
@@ -0,0 +1,152 @@
+package metrics
+
+import (
+	"github.com/dustin/go-humanize"
+	"sync"
+	"time"
+)
+
+type Metrics struct {
+	sync.RWMutex
+	startTime time.Time
+	stats     map[string]int64
+	events    []Event
+}
+
+type Event struct {
+	Timestamp         time.Time
+	TimestampReadable string
+	Category          string
+	//bin       string
+	//filename  string
+	Text string
+}
+
+func Init() Metrics {
+	m := Metrics{}
+	m.Lock()
+	m.startTime = time.Now().UTC()
+	m.stats = make(map[string]int64)
+	m.Unlock()
+	return m
+}
+
+func (m *Metrics) Uptime() time.Duration {
+	m.RLock()
+	defer m.RUnlock()
+	return time.Since(m.startTime)
+}
+
+func (m *Metrics) StartTime() time.Time {
+	m.RLock()
+	defer m.RUnlock()
+	return m.startTime
+}
+
+func (m *Metrics) GetStats() map[string]int64 {
+	m.RLock()
+	defer m.RUnlock()
+	return m.stats
+}
+
+func (m *Metrics) Get(key string) (int64, bool) {
+	m.RLock()
+	defer m.RUnlock()
+	value, found := m.stats[key]
+	return value, found
+}
+
+func (m *Metrics) IncrSet(key string, i int64) int64 {
+	newValue := i
+	m.Lock()
+	defer m.Unlock()
+	currentValue, found := m.stats[key]
+	if found {
+		newValue = currentValue + i
+	}
+	m.stats[key] = newValue
+	return newValue
+}
+
+func (m *Metrics) Incr(key string) int64 {
+	m.Lock()
+	defer m.Unlock()
+	currentValue, _ := m.stats[key]
+	newValue := currentValue + 1
+	m.stats[key] = newValue
+	return newValue
+}
+
+func (m *Metrics) Decr(key string) int64 {
+	m.Lock()
+	defer m.Unlock()
+	currentValue, _ := m.stats[key]
+	newValue := currentValue - 1
+	m.stats[key] = newValue
+	return newValue
+}
+
+//func (m *Metrics) Event(category string, text string) {
+//	m.Lock()
+//	defer m.Unlock()
+//	e := Event{
+//		Timestamp: time.Now().UTC(),
+//		Category: category,
+//		Text: text,
+//	}
+//	m.events = append(m.events, e)
+//
+//	if len(m.events) > 10000 {
+//		_, m.events = m.events[len(m.events)-1], m.events[:len(m.events)-1]
+//	}
+//}
+
+func (m *Metrics) Event(category string, text string) {
+	m.Lock()
+	defer m.Unlock()
+	e := Event{
+		Timestamp: time.Now().UTC(),
+		Category:  category,
+		Text:      text,
+	}
+	m.events = append(m.events, e)
+
+	if len(m.events) > 10000 {
+		_, m.events = m.events[len(m.events)-1], m.events[:len(m.events)-1]
+	}
+}
+
+func (m *Metrics) GetEvents(limit int) []Event {
+	m.RLock()
+	defer m.RUnlock()
+	var r []Event
+	counter := 0
+	for _, e := range m.events {
+		counter += 1
+		e.TimestampReadable = humanize.Time(e.Timestamp)
+		r = append(r, e)
+		if limit != 0 && limit == counter {
+			break
+		}
+	}
+	return r
+}
+
+func (m *Metrics) GetEventCategory(category string, limit int) []Event {
+	m.RLock()
+	defer m.RUnlock()
+	var r []Event
+	counter := 0
+	for i := len(m.events) - 1; i >= 0; i-- {
+		e := m.events[i]
+		if e.Category == category {
+			counter += 1
+			e.TimestampReadable = humanize.Time(e.Timestamp)
+			r = append(r, e)
+			if limit != 0 && limit == counter {
+				break
+			}
+		}
+	}
+	return r
+}
---
+++ app/metrics/metrics_test.go
@@ -0,0 +1,109 @@
+package metrics
+
+import (
+	"testing"
+	//"time"
+	"os"
+	"strconv"
+)
+
+var (
+	m Metrics
+)
+
+func TestMain(test *testing.M) {
+	m = Init()
+	retCode := test.Run()
+	os.Exit(retCode)
+}
+
+func TestIncrSet(t *testing.T) {
+	value, found := m.Get("foo")
+	if found == true {
+		t.Fatal("The key was unexpectedly found")
+	}
+
+	if value != 0 {
+		t.Fatal("The value is not 0")
+	}
+
+	value = m.IncrSet("foo", 1)
+	if value != 1 {
+		t.Fatal("The value is not 1")
+	}
+
+	value = m.IncrSet("foo", 1)
+	if value != 2 {
+		t.Fatal("The value is not 2")
+	}
+}
+
+func TestGet(t *testing.T) {
+	value, found := m.Get("foo")
+	if found == false {
+		t.Fatal("The key does not exist")
+	}
+	if value != 2 {
+		t.Fatal("The value is not 2. Weird.")
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	stats := m.GetStats()
+	if len(stats) != 1 {
+		t.Fatal("The number of stats is not 1")
+	}
+	if stats["foo"] != 2 {
+		t.Fatal("The value is not 2. Weird.")
+	}
+}
+
+func TestIncr(t *testing.T) {
+	if m.Incr("bar") != 1 {
+		t.Fatal("The value is not 1")
+	}
+	if m.Incr("bar") != 2 {
+		t.Fatal("The value is not 2")
+	}
+	if m.Incr("bar") != 3 {
+		t.Fatal("The value is not 3")
+	}
+}
+
+func TestDecr(t *testing.T) {
+	if m.Decr("bar") != 2 {
+		t.Fatal("The value is not 2")
+	}
+	if m.Decr("bar") != 1 {
+		t.Fatal("The value is not 1")
+	}
+	if m.Decr("bar") != 0 {
+		t.Fatal("The value is not 0")
+	}
+}
+
+func TestEvent(t *testing.T) {
+	category := "foo"
+	text := "some bar happened"
+	m.Event(category, text)
+	if len(m.events) != 1 {
+		t.Fatal("Unexpected number of events. Not 1.")
+	}
+	for i := 0; i <= 20000; i++ {
+		m.Event(category, text+": "+strconv.Itoa(i))
+	}
+	if len(m.events) != 10000 {
+		t.Fatal("Unexpected number of events. Not 10000.")
+	}
+}
+
+func TestGetEvents(t *testing.T) {
+	events := m.GetEvents(0)
+	if len(events) != 10000 {
+		t.Fatal("Unexpected number of events. Not 10000.")
+	}
+	events = m.GetEvents(100)
+	if len(events) != 100 {
+		t.Fatal("Unexpected number of events. Not 100.")
+	}
+}
